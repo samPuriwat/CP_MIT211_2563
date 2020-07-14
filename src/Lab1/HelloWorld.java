c2ff6f2b81de5181253a8bf871737d628a2c5fcb
samPuriwat <sam.puriwat@gmail.com>
2020-07-14 17:54:21 +0700

    add code 2 lines

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
         System.out.println("Puriwat Lertkrai");
+        System.out.println("MT");
+        System.out.println("RUTS");
+
 
     }
 
