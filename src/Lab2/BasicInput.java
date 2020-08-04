85421daf8e23dda3d26207b9d7dde66b2b58b17d
samPuriwat <sam.puriwat@gmail.com>
2020-08-04 19:01:27 +0700

    Lab 2 : basic input

---
+++ src/Lab2/BasicInput.java
@@ -0,0 +1,39 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class BasicInput {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name;
+//        System.out.print("What is your name? : ");
+//        name = scanner.nextLine();
+//        System.out.println("Your name is "+name);
+//
+//        System.out.print("What is your major? : ");
+//        String major = scanner.nextLine();
+//        System.out.println("Your major is "+major);
+
+        System.out.print("How old are you?: ");
+        int age = Integer.parseInt(scanner.nextLine());
+
+        //age
+        //email
+
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+}//class
