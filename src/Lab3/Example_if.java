e50ff04fba7343f14d4afb50e8ffc5bd1b87c8f3
samPuriwat <sam.puriwat@gmail.com>
2020-08-11 18:43:00 +0700

    Lab 3: if,if-else,if-else-if

---
+++ src/Lab3/Example_if.java
@@ -0,0 +1,22 @@
+package Lab3;
+
+public class Example_if {
+
+    public static void main(String[] args) {
+
+        int x,y;
+
+        x = 10;
+        y = 20;
+
+        if (x<y) {
+            //.....statement of if
+            System.out.println("x grater than y.");
+        }
+           System.out.println("Good bye.");
+
+
+
+
+    }//main
+}//class
